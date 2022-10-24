ae9957a2e5dcf4b5415283cf03489e85b6795304
afn <afnzmn@gmail.com>
2022-10-24 11:09:31 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let clock = document.getElementById("clock");
+
+clock.textContent = new Date().toLocaleTimeString();
+setInterval(() => {
+  clock.textContent = new Date().toLocaleTimeString();
+}, 1000);
+
+
+let today = new Date();
+let currentHour = today.getHours();
+let greetingMessage;
+
+if (currentHour < 12) {
+  greetingMessage = "Good morning, ";
+} else if (currentHour < 10) {
+  greetingMessage = "Good afternoon, ";
+} else {
+  greetingMessage = "Good evening, ";
+}
+
+let greeting = document.getElementById("greeting");
+let date = document.getElementById("date");
+
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December",];
+const yyyy = today.getFullYear();
+let mm = today.getMonth() + 1;
+let dd = today.getDate();
+
+if (dd < 10) dd = "0" + dd;
+
+const formattedToday = months[today.getMonth()] + " " + dd + ", " + yyyy;
+
+greeting.textContent = greetingMessage;
+date.textContent = formattedToday;  
